46138e088acc450e34c1bf0be27851da5b7f085c
Oksana Mystrikowa <oksana.mystrikowa@skygate.io>
2020-03-20 13:21:13 +0100

    added calculator tests

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -5,14 +5,6 @@ const {
 const calculatorPageObject = require('../support/page_objects/calculator.js')
 
 describe('calculator', () => {
-    // const percentage = {
-    //     calcium: 71.4691,
-    //     phosphorus: 43.642
-    // }
-    // const molarMas = {
-    //     calcium: 40.078,
-    //     phosphorus: 30.973762
-    // }
 
     const elements = {
         phosphorus: {
@@ -40,7 +32,7 @@ describe('calculator', () => {
             molarMass: 40.078,
             name: 'Calcium'
         }
-        
+
     }
 
     before(() => {
@@ -49,7 +41,6 @@ describe('calculator', () => {
             .contains('calculator')
             .click()
     })
-    // const elementName = 'calcium'
 
     for (const elementName in elements) {
         context(`when user chosen element: "${elementName}"`, () => {
@@ -62,61 +53,36 @@ describe('calculator', () => {
                 const amountOfElementKgHa = (amountOfElementGHa / 1000)
                 const amountOfElementMlHa = (amountOfElementGHa * 22.4 / elements[elementName].molarMass)
                 const amountOfElementLHa = (amountOfElementKgHa * 22.4 / elements[elementName].molarMass)
-        
+
                 before(() => {
-                    // cy.get('.ant-select').click()
-                    // cy.get('.ant-select-dropdown-menu').contains(elements[elementName].name).click()
                     calculatorPageObject.selectChemicalElement(elements[elementName].name)
-                    // cy.get('input[name="compoundWeightG"]').clear().type(amountOfChemicalCompoundGHa)
                     calculatorPageObject.fillCompoundWeightG(amountOfChemicalCompoundGHa.toFixed(2))
                 })
-        
+
                 it('should properly convert to "The amount of chemical compound" kg/ha', () => {
-        
-                    // cy
-                    //     .wait(3000)
-                    //     .get('input[name="compoundWeightKg"]')
-                    //     .should('have.value', amountOfChemicalCompoundKgHa)
-        
                     calculatorPageObject
                         .getInputOfAmountChemicalCompoundKgHa()
                         .should('have.value', amountOfChemicalCompoundKgHa.toFixed(2))
                 })
-        
+
                 it('should properly convert to "The amount of the element" g/ha', () => {
-                    // cy
-                    //     .get('input[name="weight"]')
-                    //     .should('have.value', amountOfElementGHa)
-        
                     calculatorPageObject
                         .getInputOfAmountOfElementGHa()
                         .should('have.value', amountOfElementGHa.toFixed(2))
                 })
-        
+
                 it('should properly convert to "The amount of the element" kg/ha', () => {
-                    // cy
-                    //     .get('input[name="weightKg"]')
-                    //     .should('have.value', amountOfElementKgHa)
-        
                     calculatorPageObject
                         .getInputOfAmountOfElementKgHa()
                         .should('have.value', amountOfElementKgHa.toFixed(2))
                 })
-        
+
                 it('should properly convert to "The amount of the element" ml/ha', () => {
-                    // cy
-                    //     .get('#gatsby-focus-wrapper > div > main > div > div > div > div:nth-child(2) > div:nth-child(3) > div:nth-child(4) > div:nth-child(1) > input')
-                    //     .should('have.value', amountOfElementMlHa)
-        
                     calculatorPageObject
                         .getInputOfAmountOfElementMlHa()
                         .should('have.value', amountOfElementMlHa.toFixed(2))
                 })
                 it('should properly convert to "The amount of the element" l/ha', () => {
-                    // cy
-                    //     .get('#gatsby-focus-wrapper > div > main > div > div > div > div:nth-child(2) > div:nth-child(3) > div:nth-child(4) > div:nth-child(2) > input')
-                    //     .should('have.value', amountOfElementLHa)
-        
                     calculatorPageObject
                         .getInputOfAmountOfElementLHa()
                         .should('have.value', amountOfElementLHa.toFixed(2))
